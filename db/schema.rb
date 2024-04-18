387abb2888b12676fcd06df91c464246d6f5f744
Nikolaos Emmanouilidis <121699424+NikEmman@users.noreply.github.com>
2024-04-18 19:25:04 +0300

    Add posts mvc

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_04_18_161501) do
+ActiveRecord::Schema[7.1].define(version: 2024_04_18_162356) do
+  create_table "posts", force: :cascade do |t|
+    t.text "body"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
